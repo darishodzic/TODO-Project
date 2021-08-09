bc6829490113ded64496bd77c28aeaf79b28aabb
Daris <daris.hodzic.dh@gmail.com>
2021-08-09 12:33:36 +0200

    finished filter and started creating local storage

--- app.js
+++ app.js
@@ -2,11 +2,13 @@
 const todoInput=document.querySelector(".todo-input")
 const todoButton=document.querySelector(".todo-button")
 const todoList=document.querySelector(".todo-list")
+const filterOption=document.querySelector(".filter-todo")
 
 
 //Event listeners
 todoButton.addEventListener("click",addTodo)
 todoList.addEventListener("click",deleteCheck)
+filterOption.addEventListener("click",filterTodo)
 //Functions
 function addTodo(event) {
     //prevent form from submitting
@@ -19,6 +21,8 @@ function addTodo(event) {
     newTodo.innerText=todoInput.value
     newTodo.classList.add("todo-item")
     todoDiv.appendChild(newTodo)
+    // add todo to local storage
+    saveLocalTodos(todoInput.value)
     //check mark button
     const completedButton = document.createElement("button")
     completedButton.innerHTML = "<i class='fas fa-check'></i>"
@@ -51,4 +55,40 @@ function deleteCheck(e) {
         const todo = item.parentElement
         todo.classList.toggle("completed")
     }
+}
+function filterTodo(e) {
+    const todos = todoList.childNodes
+    todos.forEach(function(todo){
+        console.log(todo);
+        switch (e.target.value) {
+            case "all":
+                todo.style.display = "flex"
+                break;
+            case "completed":
+                if(todo.classList.contains("completed")){
+                    todo.style.display = "flex"
+                }else{
+                    todo.style.display = "none"
+                } 
+                break
+            case "uncompleted":
+                if(!todo.classList.contains("completed")){
+                    todo.style.display = "flex"
+                }else{
+                    todo.style.display = "none"
+                } 
+                break
+        }
+    })
+}
+function saveLocalTodos(todo) {
+    //check
+    let todos
+    if (localStorage.getItem("todos")=== null) {
+        todos=[]
+    }else{
+        todos = JSON.parse(localStorage.getItem("todos"))
+    }
+    todos.push(todo)
+    localStorage.setItem("todos",JSON.stringify(todos))
 }
